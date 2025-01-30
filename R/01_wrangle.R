2b1f3b1644d02f80656ec33080c5084f99607887
clemdelrevo <clement.delamare@live.fr>
2025-01-30 17:39:59 +0400

    publish real actions

--- R/01_wrangle.R
+++ R/01_wrangle.R
@@ -13,6 +13,9 @@ wrangle_initiatives <- function(initiatives) {
   initiatives$image[initiatives$type == "Lutte Espèces Exotiques Envahissantes"] <- "images/EEE.png"
   initiatives$image[initiatives$type == "Suivis scientifiques"] <- "images/loupe.png"
   initiatives$image[initiatives$type == "Ramassage de déchets"] <- "images/dechets.jpg"
+  initiatives$image[initiatives$type == "Rivière"] <- "images/riviere.jpg"
+  initiatives$image[initiatives$type == "Sensibilisation"] <- "images/sensibilisation.jpg"
+  initiatives$image[initiatives$type == "Déblaiement de sentier"] <- "images/deblaiement.jpg"
   
   return(initiatives)
   
