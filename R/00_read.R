0dd02f69d4c7aba8d96978988eed87f3e9f726a4
clemdelrevo <clement.delamare@live.fr>
2025-02-13 15:22:27 +0300

    add map

--- R/00_read.R
+++ R/00_read.R
@@ -10,3 +10,15 @@ read_initiatives <- function(initiative_path) {
   read.csv(initiative_path)
   
 }
+
+get_bversant_path <- function() {
+  
+  "data/BASSIN_VERSANT_TOPOGRAPHIQUE.shp"
+  
+}
+
+read_bversant <- function(bversant_path) {
+  
+  sf::read_sf(bversant_path)
+  
+}
--- analyses/pipeline.R
+++ analyses/pipeline.R
@@ -14,7 +14,9 @@ tar_source()
 list(
   
    tar_target(initiative_path, get_initiative_path(), format = "file")
+  ,tar_target(bversant_path, get_bversant_path(), format = "file") 
   ,tar_target(initiatives, read_initiatives(initiative_path))
   ,tar_target(winitiatives, wrangle_initiatives(initiatives))
+  ,tar_target(bversant, read_bversant(bversant_path))
   
 )
